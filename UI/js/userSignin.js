59db5d47bc7162dfa508e91edb2bef241c318c8c
jideajayi11 <jideajayi11@gmail.com>
2018-10-04 19:53:18 -0700

    [Feature #160909673] finishes user signin

--- UI/js/userSignin.js
+++ UI/js/userSignin.js
@@ -1,26 +1,23 @@
 const signinForm = document.forms['signinForm'];
 signinForm.addEventListener('submit', (event) => {
 	event.preventDefault();
-	const uri = '/api/v1/auth/login';
-  const methodF = 'POST';
-  const bodyF = {
+	const fetchUrl = '/api/v1/auth/login';
+  const fetchMethod = 'POST';
+  const fetchBody = {
 		email: signinForm.email.value,
 		password: signinForm.password.value
   };
-	fetch(requestFetch(uri, methodF, bodyF))
+	fetch(requestFetch(fetchUrl, fetchMethod, fetchBody))
 	.then(resp => resp.json())
 	.then((data) => {
-		console.log(data);
 		if(data.status === 'success') {
 			window.location.href = 'viewFood.html';
 			localStorage.setItem('fastFoodToken', data.token);
-			localStorage.setItem('userName', data.data.fullName);
 		} else {
 			document.querySelector('#errorMessage').innerHTML = data.message;
 		}
 	})
 	.catch((error) => {
-		console.log(error);
 		document.querySelector('#errorMessage').innerHTML = `
 			Something is not right. Try checking your connection.`;
 	});
