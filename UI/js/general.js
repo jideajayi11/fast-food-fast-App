9346e09e911d9e304d6dd255537d050d16769ca7
jideajayi11 <jideajayi11@gmail.com>
2018-10-22 17:26:32 -0700

    [Feature #161277835] create popup alert ui

--- UI/js/general.js
+++ UI/js/general.js
@@ -1,4 +1,4 @@
-function requestFetch(fetchUrl, fetchMethod, fetchBody = {}) {
+requestFetch = (fetchUrl, fetchMethod, fetchBody = {}) => {
   const baseURL = 'https://fast-food-fast-delivery.herokuapp.com';
 	showLoading();
 
@@ -14,7 +14,7 @@ function requestFetch(fetchUrl, fetchMethod, fetchBody = {}) {
 
 	return request = new Request(baseURL + fetchUrl, {
 		method: fetchMethod,
-		headers: new Headers({	
+		headers: new Headers({
 			'Content-Type': 'application/json',
 			'x-access-token': localStorage.getItem('fastFoodToken'),
 		}),
@@ -22,7 +22,7 @@ function requestFetch(fetchUrl, fetchMethod, fetchBody = {}) {
 	});
 }
 
-function createLoader () {
+createLoader = () => {
 	const loading = document.createElement('div');
 	const image = document.createElement('img');
 	image.setAttribute('src', 'images/download.png');
@@ -33,11 +33,43 @@ function createLoader () {
 }
 createLoader();
 
-function showLoading () {
+showLoading = () => {
 	const load = document.querySelector('.loading');
 	load.style.display = 'flex';
 }
-function hideLoading () {
+hideLoading = () => {
 	const unload = document.querySelector('.loading');
 	unload.style.display = 'none';
 }
+
+popupAlert = () => {
+	const popup = document.createElement('div');
+	popup.setAttribute('class', 'popupAlertOverlay');
+	const child = document.createElement('div');
+	child.setAttribute('class', 'popup');
+	let grandChild = document.createElement('div');
+	grandChild.setAttribute('class', 'popup-head');
+	grandChild.innerHTML = `<strong id="popupAlertTitle"></strong>`;
+	child.appendChild(grandChild);
+	grandChild = document.createElement('div');
+	grandChild.setAttribute('class', 'popup-body');
+	grandChild.innerHTML = `<span id="popupAlertMessage"></span>
+						<div class="mt-20 spread-out">
+							<button id="popupAlertOK">OK</button>
+            </div>`;
+	child.appendChild(grandChild);
+	popup.appendChild(child);
+	document.querySelector('body').appendChild(popup);
+}
+popupAlert ();
+showPopupAlert = (title, message) => {
+	document.getElementById('popupAlertTitle').innerHTML = title;
+	document.getElementById('popupAlertMessage').innerHTML = message;
+	const popup = document.querySelector('.popupAlertOverlay');
+	popup.style.display = 'flex';
+	const okBtn = document.getElementById('popupAlertOK');
+	okBtn.addEventListener('click', (event) => {
+		popup.style.display = 'none';
+	});
+}
+showPopupAlert('test', 'This is a popup alert test.');
